89d854e68c39492b10a867e90d02dc26570540ef
Wexengos <gnos2007@gmail.com>
2025-02-02 22:20:35 -0300

    add env variables

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,8 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "https://api.discogs.com",
     headers: {
-        Authorization: "Discogs token=lPTOruJYLmPnDSqWmliozzuVvaCqMNPJXXQdpmNf"
+        Authorization: `Discogs token=${import.meta.env.VITE_API_KEY}`
     }
-   });
+});
    
    export default api
